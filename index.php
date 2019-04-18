5fa82fc66984b7c92f09d6f83f4e2d2aad669808
Jamalbek <32876973+Zhamalbek@users.noreply.github.com>
2019-04-18 17:58:17 +0600

    end project

--- index.php
+++ index.php
@@ -1,19 +1,38 @@
 <?
 if(empty($_GET)) exit("GET запроса нету");
 
-function counter(){
-    $fp = fopen("counterlog.txt", "r") or die("не удалось открыть файл");; 
-    $count = fread($fp, 1024); 
-    fclose($fp); 
-    
-    $count++; 
-    
-    $fp = fopen("counterlog.txt", "w") or die("не удалось открыть файл");; 
-    fwrite($fp, $count); 
-    fclose($fp); 
-}
-$fd = fopen($count.".dat", 'w+') or die("не удалось открыть файл");
+if($_GET['p']!=''){
+    function counter($t){
+        if (!file_exists($t)) {
+            $fp = fopen($t, "w");
+            fwrite($fp, '0'); 
+            fclose($fp);
+        }
+        $fp = fopen($t, "r") or die("не удалось открыть файл");; 
+        $count = fread($fp, 1024); 
+        fclose($fp); 
+        
+        $count++; 
+        
+        $fp = fopen($t, "w") or die("не удалось открыть файл");; 
+        fwrite($fp, $count); 
+        fclose($fp);
+        
+        return $count;
+    }
 
-$str = $_GET["p"];
-fwrite($fd, $str);
-fclose($fd);
+    $fd = fopen(counter("counterlog.txt").".dat", 'w+') or die("не удалось открыть файл");
+
+    $str = $_GET["p"];
+    fwrite($fd, $str);
+    fclose($fd);
+}
+// редирект
+if($_GET['q']!=''){
+    $get = $_GET['q']*1;
+    $fp = fopen($get.".dat", "r") or die("не удалось открыть файл");; 
+    $url = fread($fp, 1024); 
+    fclose($fp); 
+    
+    header("Location: $url");
+}
