81e64e04a63a1fe756397a824eb216633cb21c91
Jamalbek <32876973+Zhamalbek@users.noreply.github.com>
2019-04-18 17:57:43 +0600

    save file

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?
 if(empty($_GET)) exit("GET запроса нету");
-require_once('tests/autoloader.php');
 
-require_once('settings.config.php');          // Define db configuration arrays here
-require_once('DBConnection.php');             // Include this file
+function counter(){
+    $fp = fopen("counterlog.txt", "r") or die("не удалось открыть файл");; 
+    $count = fread($fp, 1024); 
+    fclose($fp); 
+    
+    $count++; 
+    
+    $fp = fopen("counterlog.txt", "w") or die("не удалось открыть файл");; 
+    fwrite($fp, $count); 
+    fclose($fp); 
+}
+$fd = fopen($count.".dat", 'w+') or die("не удалось открыть файл");
+
+$str = $_GET["p"];
+fwrite($fd, $str);
+fclose($fd);
